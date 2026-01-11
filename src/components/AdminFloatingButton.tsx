e5a6368241daa6aa04629276699d324025ab9013
kellyworkos <kellyworkos00@gmail.com>
2026-01-12 00:17:22 +0300

    Move admin button to bottom-left to avoid overlap with cart

--- src/components/AdminFloatingButton.tsx
+++ src/components/AdminFloatingButton.tsx
@@ -10,16 +10,16 @@ export default function AdminFloatingButton() {
   return (
     <Link
       href="/admin"
-      className="fixed bottom-6 right-6 z-50 group"
+      className="fixed bottom-6 left-6 z-50 group"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <div className="relative">
         {/* Tooltip */}
         {isHovered && (
-          <div className="absolute bottom-full right-0 mb-2 px-3 py-2 bg-gray-900 text-white text-sm rounded-lg whitespace-nowrap animate-fade-in">
+          <div className="absolute bottom-full left-0 mb-2 px-3 py-2 bg-gray-900 text-white text-sm rounded-lg whitespace-nowrap animate-fade-in">
             Admin Panel
-            <div className="absolute top-full right-4 w-0 h-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-gray-900"></div>
+            <div className="absolute top-full left-4 w-0 h-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-gray-900"></div>
           </div>
         )}
         
